65ae28d02c197bbb45158fd9741462ba6a817e63
Sidney Andrews <sidney@wordpane.com>
2026-07-21 16:50:29 -0300

    update

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mindubadigital.com.br',
-  integrations: [sitemap({ filter: (page) => !page.includes('/comparativo') })],
+  integrations: [sitemap()],
   build: {
     inlineStylesheets: 'always',
   },
